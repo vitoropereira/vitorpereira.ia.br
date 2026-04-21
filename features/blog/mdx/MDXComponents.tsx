335b21de4f31070a725a81b1e4a236d5e906e88b
vitoropereira <vop1234@hotmail.com>
2026-04-21 15:59:28 -0300

    feat(blog): add MDX component registry

---
+++ features/blog/mdx/MDXComponents.tsx
@@ -0,0 +1,16 @@
+import { Callout } from "./Callout";
+import { Video } from "./Video";
+import { ImageWithBlur } from "./ImageWithBlur";
+import { Tweet } from "./Tweet";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+
+export const mdxComponents = {
+  Callout,
+  Video,
+  Image: ImageWithBlur,
+  Tweet,
+  Tabs,
+  TabsList,
+  TabsTrigger,
+  TabsContent,
+};
